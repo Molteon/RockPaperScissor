112783250f9383275e9c0fc240f63b2c3407d451
Molteon <imhr@outlook.com>
2017-01-17 00:19:21 +0300

    fixed bugs.

--- scripts/main.js
+++ scripts/main.js
@@ -1,64 +1,72 @@
-//Computer Choice is randomized then saved into a variable "computerChoice";
-var computerChoice = Math.random();
-if (computerChoice <= 0.33) {
-	var computerChoice = "Paper";
-}
-else if (computerChoice >= 0.34 && computerChoice <= 0.66)
-{
-	var computerChoice = "Rock";
-}
-else{
-	var computerChoice = "Scissor";
+//Computer Choice Function user to randomize the selection of the computer then it will be saved into a variable "computerChoice";
+var computerChoiceFunction = function(){
+	var computerChoice = Math.random();
+	if (computerChoice <= 0.33) {
+		 var computerChoice = "Paper";
+		 return computerChoice;
+	}
+	else if (computerChoice >= 0.34 && computerChoice <= 0.66)
+	{
+		 var computerChoice = "Rock";
+		 return computerChoice;
+	}
+	else{
+		 var computerChoice = "Scissor";
+		 return computerChoice;
+	}
 }
 
 
 //If user clicks on the button Rock, it will compare Rock against computerChoice;
 
 var rock = function(){
+	var computerChoice = computerChoiceFunction();
 	if(computerChoice == "Scissor")
 	{
-		document.getElementById("userChoiceForm").elements["result"].value = "Win";
+		document.getElementById("userChoiceForm").elements["result"].value = "You Win! Computer chose " + computerChoice;
 	}
 
 	else if (computerChoice == "Rock") {
-		document.getElementById("userChoiceForm").elements["result"].value = "Tie";
+		document.getElementById("userChoiceForm").elements["result"].value = "Its a Tie! Computer chose " + computerChoice;
 	}
 	else
 	{
-		document.getElementById("userChoiceForm").elements["result"].value = "Lost";
+		document.getElementById("userChoiceForm").elements["result"].value = "You Lost. Computer chose " + computerChoice;
 	}
 }
 
 //If user clicks on the button Paper, it will compare Paper against computerChoice;
 
 var paper = function(){
+	var computerChoice = computerChoiceFunction();
 	if(computerChoice == "Scissor")
 	{
-		document.getElementById("userChoiceForm").elements["result"].value = "Lost";
+		document.getElementById("userChoiceForm").elements["result"].value = "You Lost. Computer chose " + computerChoice;
 	}
 
 	else if (computerChoice == "Rock") {
-		document.getElementById("userChoiceForm").elements["result"].value = "Win";
+		document.getElementById("userChoiceForm").elements["result"].value = "You Win! Computer chose " + computerChoice;
 	}
 	else
 	{
-		document.getElementById("userChoiceForm").elements["result"].value = "Tie";
+		document.getElementById("userChoiceForm").elements["result"].value = "Its a Tie! Computer chose " + computerChoice;
 	}
 }
 
 //If user clicks on the button Scissor, it will compare Scissor against computerChoice;
 
 var scissor = function(){
+	var computerChoice = computerChoiceFunction();
 	if(computerChoice == "Scissor")
 	{
-		document.getElementById("userChoiceForm").elements["result"].value = "Tie";
+		document.getElementById("userChoiceForm").elements["result"].value = "Its a Tie! Computer chose " + computerChoice;
 	}
 
 	else if (computerChoice == "Rock") {
-		document.getElementById("userChoiceForm").elements["result"].value = "Lost";
+		document.getElementById("userChoiceForm").elements["result"].value = "You Lost. Computer chose " + computerChoice;
 	}
 	else
 	{
-		document.getElementById("userChoiceForm").elements["result"].value = "Win";
+		document.getElementById("userChoiceForm").elements["result"].value = "You Win! Computer chose " + computerChoice;
 	}
 }
